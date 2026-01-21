ac896a4442639ab618b761b03260034e308fc17d
Yu-Jin-Jeong <herrjyj@gmail.com>
2026-01-22 00:12:42 +0900

    chore: mark wf build id

--- src/pages/wf/WfHome.tsx
+++ src/pages/wf/WfHome.tsx
@@ -3,6 +3,9 @@ export function WfHome() {
     <section>
       <h1>Wireframe Capture Home</h1>
       <p>Start here to explore the wireframe capture experience.</p>
+      <div style={{ position: 'fixed', bottom: 8, right: 8, fontSize: 12, opacity: 0.6 }}>
+        build: 2026-01-22-01
+      </div>
     </section>
   );
 }
